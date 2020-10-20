8b7f9adc7b8686f11c2abb490f94c936ac048d7d
Kris Torres <kristorresdayo@gmail.com>
2020-10-20 11:38:01 -0700

    Provide more insightful ContentView previews
    I provided previews for a `ContentView` on the following devices:
    
      * iPhone SE (1st generation)
      * iPhone X
      * iPhone X in Dark Mode
      * iPad Air (4th generation)

--- Tensai/Views/ContentView.swift
+++ Tensai/Views/ContentView.swift
@@ -13,8 +13,21 @@ struct ContentView: View {
 #if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        Group {
+        let viewName = "Content View"
+        return Group {
             ContentView()
+                .previewDevice("iPhone SE (1st generation)")
+                .previewDisplayName("\(viewName) — iPhone SE 1")
+            ContentView()
+                .previewDevice("iPhone X")
+                .previewDisplayName("\(viewName) — iPhone X")
+            ContentView()
+                .previewDevice("iPhone X")
+                .preferredColorScheme(.dark)
+                .previewDisplayName("\(viewName) — iPhone X (Dark Mode)")
+            ContentView()
+                .previewDevice("iPad Air (4th generation)")
+                .previewDisplayName("\(viewName) — iPad Air 4")
         }
     }
 }

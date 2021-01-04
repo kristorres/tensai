7db680b5530aef1e98f64ca20bc44a92b600c46f
Kris Torres <kristorresdayo@gmail.com>
2021-01-04 11:00:35 -0800

    Create the LocalStorageKey struct to store values

---
+++ Tensai/Storage/LocalStorageKey.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+/// A valid key to store values in the shared defaults object.
+struct LocalStorageKey {
+    
+    /// The key to store a trivia quiz configuration locally.
+    let triviaQuizConfig = "TriviaQuizConfig"
+}

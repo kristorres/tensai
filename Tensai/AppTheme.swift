aefe568ee388d6f259819957f1530f4df2f5625a
Kris Torres <kristorresdayo@gmail.com>
2021-08-18 16:39:48 -0700

    Refactor the color palette in the app theme
    I actually do not need the “Pressed” variants of the “Primary”,
    “Secondary”, and “Danger” colors because I can just overlay them with a
    translucent white layer.

--- Tensai/AppTheme.swift
+++ Tensai/AppTheme.swift
@@ -6,53 +6,28 @@ import SwiftUI
 /// customize design aspects such as the colors, typography, and more.
 struct AppTheme {
     
+    /// A main/content pair color.
+    typealias ColorPair = (mainColor: Color, contentColor: Color)
+    
     /// The color palette.
     struct ColorPalette {
+        static let primary = colorPair(name: "Primary")
+        static let secondary = colorPair(name: "Secondary")
+        static let danger = colorPair(name: "Danger")
+        static let surface = colorPair(name: "Surface")
+        static let background = colorPair(name: "Background")
         
-        /// The color displayed most frequently across the app’s screens and
-        /// components.
-        static let primary = Color("Primary")
-        
-        /// The shade of the `primary` color used when a control is pressed.
-        static let primaryWhenPressed = Color("Primary (Pressed)")
-        
-        /// The color used to accent and distinguish the app.
-        static let secondary = Color("Secondary")
-        
-        /// The shade of the `secondary` color used when a control is pressed.
-        static let secondaryWhenPressed = Color("Secondary (Pressed)")
-        
-        /// The color used to represent warnings, errors, and other destructive
-        /// actions.
-        static let danger = Color("Danger")
-        
-        /// The shade of the `danger` color used when a control is pressed.
-        static let dangerWhenPressed = Color("Danger (Pressed)")
-        
-        /// The color displayed across distinguishable surfaces.
-        static let surface = Color("Surface")
-        
-        /// The baseline background color.
-        static let background = Color("Background")
-        
-        /// The color of elements that appear “on” top of views with a `primary`
-        /// color fill.
-        static let onPrimary = Color("On Primary")
-        
-        /// The color of elements that appear “on” top of views with a
-        /// `secondary` color fill.
-        static let onSecondary = Color("On Secondary")
-        
-        /// The color of elements that appear “on” top of views with a `danger`
-        /// color fill.
-        static let onDanger = Color("On Danger")
-        
-        /// The color of elements that appear “on” top of views with a `surface`
-        /// color fill.
-        static let onSurface = Color("On Surface")
-        
-        /// The color of elements that appear “on” top of views with a
-        /// `background` color fill.
-        static let onBackground = Color("On Background")
+        /// Returns a color pair from the color resource with the specified
+        /// name.
+        ///
+        /// - Parameter name: The name of the color resource.
+        ///
+        /// - Returns:
+        ///   - mainColor:    The main color.
+        ///   - contentColor: The color of elements that appear “on” top of
+        ///                   components with a `mainColor` fill.
+        private static func colorPair(name: String) -> ColorPair {
+            return (Color(name), Color("On \(name)"))
+        }
     }
 }

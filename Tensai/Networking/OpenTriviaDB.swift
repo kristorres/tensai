6e10bfce3d888e537bf7e45eed981e5602439831
Kris Torres <kristorresdayo@gmail.com>
2021-10-14 09:52:19 -0700

    Implement the OpenTriviaDB.Query struct

--- Tensai/Networking/OpenTriviaDB.swift
+++ Tensai/Networking/OpenTriviaDB.swift
@@ -19,6 +19,69 @@ struct OpenTriviaDB {
         }
     }
     
+    /// A query for fetching trivia questions from the
+    /// [Open Trivia Database](https://opentdb.com).
+    struct Query: Codable {
+        
+        /// Creates a default database query.
+        init() {}
+        
+        /// Creates a database query with the given “property list.”
+        ///
+        /// If the “property list” is `nil` or it cannot be decoded, then this
+        /// initializer will return `nil`.
+        ///
+        /// - Parameter propertyList: The “property list” to decode.
+        init?(propertyList: Data?) {
+            guard let propertyList = propertyList else {
+                return nil
+            }
+            
+            guard let query = try? JSONDecoder().decode(
+                Query.self,
+                from: propertyList
+            ) else {
+                return nil
+            }
+            
+            self = query
+        }
+        
+        /// The default number of questions in a trivia quiz.
+        private static let defaultQuestionCount = 10
+        
+        /// The number of questions in a trivia quiz.
+        var questionCount = Self.defaultQuestionCount
+        
+        /// The category ID.
+        var categoryID: Int?
+        
+        /// The question type.
+        var questionType: Question.Kind?
+        
+        /// The API URL.
+        var apiURL: URL? {
+            var urlStringParts = [
+                "https://opentdb.com/api.php",
+                "?amount=\(questionCount)"
+            ]
+            
+            if let categoryID = categoryID {
+                urlStringParts.append("&category=\(categoryID)")
+            }
+            if let questionType = questionType {
+                urlStringParts.append("&type=\(questionType.rawValue)")
+            }
+            
+            return URL(string: urlStringParts.joined())
+        }
+        
+        /// The “property list” representation of this database query.
+        var propertyList: Data? {
+            try? JSONEncoder().encode(self)
+        }
+    }
+    
     /// A question in the [Open Trivia Database](https://opentdb.com).
     struct Question: Codable {
         

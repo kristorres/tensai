0ca89a7ebcea5963f27a4fecd2b02ed4d25afdf4
Kris Torres <kristorresdayo@gmail.com>
2021-10-15 15:11:44 -0700

    Implement the DataCenter class

---
+++ Tensai/DataCenter.swift
@@ -0,0 +1,102 @@
+import Foundation
+
+/// An object that handles data fetching.
+final class DataCenter {
+    
+    /// Creates a data center with the given URL session.
+    ///
+    /// - Parameter urlSession: The URL session. The default is a session that
+    ///                         is configured with no caching policy.
+    init(urlSession: URLSession = URLSession(configuration: .noCaching)) {
+        self.urlSession = urlSession
+    }
+    
+    /// The shared singleton data center.
+    static let shared = DataCenter()
+    
+    /// The URL session.
+    private let urlSession: URLSession
+    
+    /// Creates a new trivia quiz with the given query to the
+    /// [Open Trivia Database](https://opentdb.com).
+    ///
+    /// - Parameter query: The database query.
+    ///
+    /// - Returns: The trivia quiz.
+    func createTriviaQuiz(
+        with query: OpenTriviaDB.Query
+    ) async throws -> TriviaQuiz {
+        if query.questionCount <= 0 {
+            throw APIError.invalidQuestionCount
+        }
+        
+        let request = URLRequest(url: query.apiURL!)
+        return try await makeAPICall(
+            with: request,
+            processResponse: { (response: OpenTriviaDB.APIResponse) in
+                if response.code == 1 {
+                    throw APIError.notEnoughQuestions
+                }
+                if response.code > 0 {
+                    throw APIError.unknown
+                }
+                
+                return TriviaQuiz(questions: response.questions)
+            }
+        )
+    }
+    
+    /// Makes an API call with the given URL request.
+    ///
+    /// - Parameter request:         The URL request.
+    /// - Parameter processResponse: The closure to process the raw API
+    ///                              response body.
+    ///
+    /// - Returns: The final output returned from `processResponse`.
+    private func makeAPICall<APIResponse: Codable, Output>(
+        with request: URLRequest,
+        processResponse: (APIResponse) throws -> Output
+    ) async throws -> Output {
+        do {
+            let (data, _) = try await urlSession.data(for: request)
+            let response = try JSONDecoder().decode(
+                APIResponse.self,
+                from: data
+            )
+            return try processResponse(response)
+        }
+        catch let error as APIError {
+            throw error
+        }
+        catch is DecodingError {
+            throw APIError.cannotParseResponse
+        }
+        catch let error as NSError where error.code == NSURLErrorTimedOut {
+            throw APIError.requestTimedOut
+        }
+        catch {
+            throw APIError.unknown
+        }
+    }
+    
+    /// An error from an API call.
+    enum APIError: Error {
+        
+        /// An API error which indicates an invalid number of questions in a
+        /// trivia quiz.
+        case invalidQuestionCount
+        
+        /// An API error which indicates that there are not enough questions
+        /// from a given query.
+        case notEnoughQuestions
+        
+        /// An API error which indicates a response that could not be parsed.
+        case cannotParseResponse
+        
+        /// An API error which indicates a request that timed out.
+        case requestTimedOut
+        
+        /// An unknown API error.
+        case unknown
+    }
+}
---
+++ TensaiTests/DataCenterTests.swift
@@ -0,0 +1,122 @@
+import XCTest
+@testable import Tensai
+
+final class DataCenterTests: XCTestCase {
+    
+    func testNegativeQuestionCount() async {
+        await testInvalidQuestionCount(-1)
+    }
+    
+    func testZeroQuestionCount() async {
+        await testInvalidQuestionCount(0)
+    }
+    
+    func testRequestTimedOut() async {
+        let urlSessionConfiguration = URLSessionConfiguration.noCaching
+        urlSessionConfiguration.timeoutIntervalForResource = 0.0001
+        let urlSession = URLSession(configuration: urlSessionConfiguration)
+        let dataCenter = DataCenter(urlSession: urlSession)
+        
+        let query = OpenTriviaDB.Query()
+        
+        await testCreateTriviaQuizFail(
+            with: query,
+            dataCenter: dataCenter,
+            expectedError: .requestTimedOut
+        )
+    }
+    
+    func testInvalidCategory() async {
+        var query = OpenTriviaDB.Query()
+        query.categoryID = -1
+        
+        await testCreateTriviaQuizFail(
+            with: query,
+            expectedError: .notEnoughQuestions
+        )
+    }
+    
+    func testTriviaQuizFromDefaultQuery() async throws {
+        let query = OpenTriviaDB.Query()
+        
+        try await testCreateTriviaQuizSuccess(with: query) { triviaQuiz in
+            XCTAssertEqual(triviaQuiz.questions.count, 10)
+        }
+    }
+    
+    func testTriviaQuizWithCustomQuestionCountOnly() async throws {
+        var query = OpenTriviaDB.Query()
+        query.questionCount = 50
+        
+        try await testCreateTriviaQuizSuccess(with: query) { triviaQuiz in
+            XCTAssertEqual(triviaQuiz.questions.count, 50)
+        }
+    }
+    
+    func testTriviaQuizWithCategory() async throws {
+        var query = OpenTriviaDB.Query()
+        query.categoryID = 9
+        
+        try await testCreateTriviaQuizSuccess(with: query) { triviaQuiz in
+            let categories = triviaQuiz.questions.map {
+                $0.category
+            }
+            let uniqueCategories = Set<String>(categories)
+            
+            XCTAssertEqual(uniqueCategories.count, 1)
+        }
+    }
+    
+    func testTriviaQuizWithQuestionType() async throws {
+        var query = OpenTriviaDB.Query()
+        query.questionType = .multipleChoice
+        
+        try await testCreateTriviaQuizSuccess(with: query) { triviaQuiz in
+            let questionTypes = triviaQuiz.questions.map {
+                $0.type
+            }
+            let uniqueQuestionTypes = Set<OpenTriviaDB.Question.Kind>(
+                questionTypes
+            )
+            
+            XCTAssertEqual(uniqueQuestionTypes.count, 1)
+        }
+    }
+    
+    private func testCreateTriviaQuizFail(
+        with query: OpenTriviaDB.Query,
+        dataCenter: DataCenter = .shared,
+        expectedError: DataCenter.APIError
+    ) async {
+        do {
+            let _ = try await dataCenter.createTriviaQuiz(with: query)
+            XCTFail("No error thrown.")
+        }
+        catch let error as DataCenter.APIError where error == expectedError {
+        }
+        catch {
+            XCTFail("Incorrect error thrown.")
+        }
+    }
+    
+    private func testCreateTriviaQuizSuccess(
+        with query: OpenTriviaDB.Query,
+        testTriviaQuiz: (TriviaQuiz) -> Void
+    ) async throws {
+        let triviaQuiz = try await DataCenter.shared.createTriviaQuiz(
+            with: query
+        )
+        
+        testTriviaQuiz(triviaQuiz)
+    }
+    
+    private func testInvalidQuestionCount(_ questionCount: Int) async {
+        var query = OpenTriviaDB.Query()
+        query.questionCount = questionCount
+        
+        await testCreateTriviaQuizFail(
+            with: query,
+            expectedError: .invalidQuestionCount
+        )
+    }
+}

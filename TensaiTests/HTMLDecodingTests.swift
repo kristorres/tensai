7920c9e36c2e6a8eff7e3b3e66adb0e93ad4d246
Kris Torres <kristorresdayo@gmail.com>
2021-11-11 22:52:48 -0800

    Extend String with the htmlDecoded property

---
+++ TensaiTests/HTMLDecodingTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import Tensai
+
+final class HTMLDecodingTests: XCTestCase {
+    func testHTMLDecoding() {
+        let song = "Twice — <b>&quot;I Can&#8217;t Stop Me&quot;</b>"
+        XCTAssertEqual(song.htmlDecoded, "Twice — \"I Can’t Stop Me\"")
+    }
+}

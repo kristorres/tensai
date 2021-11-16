d718f39ef7021c3b26b1fbaa57d477ae8b3862a1
Kris Torres <kristorresdayo@gmail.com>
2021-11-15 22:35:18 -0800

    Implement the OTDBResponse struct

---
+++ Tensai/Networking/OTDBResponse.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+/// A response from an API call to the
+/// [Open Trivia Database](https://opentdb.com).
+struct OTDBResponse: Codable {
+    
+    /// The response code.
+    let code: Int
+    
+    /// The questions.
+    let questions: [OTDBQuestion]
+    
+    /// An internal type that contains the keys for encoding and decoding.
+    private enum CodingKeys: String, CodingKey {
+        case code = "response_code"
+        case questions = "results"
+    }
+}

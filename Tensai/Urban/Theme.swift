56214580bd9ed0b45f221769049d5f36e39b9e20
Kris Torres <torres-kris@outlook.com>
2022-03-09 11:46:56 -0800

    Recreate the app theme

---
+++ Tensai/Urban/Theme.swift
@@ -0,0 +1,56 @@
+import SwiftUI
+
+/// An app theme.
+///
+/// The purpose of the theme is to apply a consistent tone to the app. You can
+/// customize design aspects such as the colors, typography, and more.
+///
+/// You can use an environment value to dynamically control the theme of a
+/// SwiftUI view (and its children).
+///
+/// ```
+/// // Setting the theme
+/// MyView().environment(\.theme, .trebek)
+///
+/// // Getting the theme
+/// @Environment(\.theme) private var theme
+/// ```
+///
+/// You can also create a new theme from an already existing theme by
+/// customizing any of the properties.
+///
+/// ```
+/// var theme = UrbanTheme.trebek
+/// theme.colorPalette = UrbanColorPalette(name: "Sajak")
+/// ```
+struct UrbanTheme {
+    
+    /// The Trebek theme.
+    static let trebek = UrbanTheme(
+        colorPalette: UrbanColorPalette(name: "Trebek"),
+        shadowRadius: 4
+    )
+    
+    /// The color palette.
+    var colorPalette: UrbanColorPalette
+    
+    /// The standard shadow “radius.”
+    var shadowRadius: CGFloat
+}
+
+extension EnvironmentValues {
+    
+    /// The app theme.
+    var theme: UrbanTheme {
+        get {
+            self[UrbanThemeKey.self]
+        }
+        set {
+            self[UrbanThemeKey.self] = newValue
+        }
+    }
+    
+    private struct UrbanThemeKey: EnvironmentKey {
+        static let defaultValue = UrbanTheme.trebek
+    }
+}

78e167f729046b0d1afa31cbe7aaa6a0e96425e8
Kris Torres <torres-kris@outlook.com>
2022-03-10 11:39:24 -0800

    Define the Torres blue and Torres gold colors

--- Tensai/Urban/Palette.swift
+++ Tensai/Urban/Palette.swift
@@ -106,6 +106,16 @@ struct UrbanPalette {
 
 extension Color {
     
+    static let torresBlue = Color.dynamic(
+        light: Color(red: 0.102, green: 0.3922, blue: 0.8431),
+        dark: Color(red: 0.5529, green: 0.7961, blue: 1)
+    )
+    
+    static let torresGold = Color.dynamic(
+        light: Color(red: 1, green: 0.7098, blue: 0),
+        dark: Color(red: 0.9961, green: 0.8863, blue: 0.5137)
+    )
+    
     /// Creates a dynamic color with light and dark variants.
     ///
     /// - Parameter lightVariant: The light variant.

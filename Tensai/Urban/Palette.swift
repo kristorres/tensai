7e6a9d1812492dc5a4f302a9c7046781bb47f5fc
Kris Torres <torres-kris@outlook.com>
2022-03-09 20:29:33 -0800

    Implement static dynamic method in Color struct

--- Tensai/Urban/Palette.swift
+++ Tensai/Urban/Palette.swift
@@ -103,3 +103,24 @@ struct UrbanPalette {
         return (mainColor, contentColor)
     }
 }
+
+extension Color {
+    
+    /// Creates a dynamic color with light and dark variants.
+    ///
+    /// - Parameter lightVariant: The light variant.
+    /// - Parameter darkVariant:  The dark variant.
+    ///
+    /// - Returns: The dynamic color.
+    static func dynamic(
+        light lightVariant: Color,
+        dark darkVariant: Color
+    ) -> Color {
+        return Color(
+            uiColor: UIColor { traitCollection in
+                let darkModeIsOn = (traitCollection.userInterfaceStyle == .dark)
+                return UIColor(darkModeIsOn ? darkVariant : lightVariant)
+            }
+        )
+    }
+}

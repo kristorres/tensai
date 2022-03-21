d8f4063b3e1cd8a9020e18c0d465342ebb6fc469
Kris Torres <torres-kris@outlook.com>
2022-03-21 16:09:12 -0700

    Implement the UrbanPaper component

---
+++ Tensai/Urban/Components/Paper.swift
@@ -0,0 +1,111 @@
+import SwiftUI
+
+/// A simple paper element that displays content.
+struct UrbanPaper<Content>: View where Content: View {
+    
+    /// Creates a paper element with the given content.
+    ///
+    /// - Parameter hasBorder: Indicates whether the paper element has a border.
+    ///                        The default is `false`.
+    /// - Parameter hasShadow: Indicates whether the paper element has a shadow.
+    ///                        The default is `true`.
+    /// - Parameter content:   The closure to render the content of the paper
+    ///                        element.
+    init(
+        border hasBorder: Bool = false,
+        shadow hasShadow: Bool = true,
+        content: @escaping () -> Content
+    ) {
+        self.hasBorder = hasBorder
+        self.hasShadow = hasShadow
+        self.content = content
+    }
+    
+    /// Indicates whether the paper element has a border.
+    private let hasBorder: Bool
+    
+    /// Indicates whether the paper element has a shadow.
+    private let hasShadow: Bool
+    
+    /// The closure to render the content of the paper element.
+    private let content: () -> Content
+    
+    /// The app theme.
+    @Environment(\.theme) private var theme
+    
+    var body: some View {
+        content()
+            .foregroundColor(theme.palette.surface.content)
+            .background(background)
+    }
+    
+    /// The background of the paper element.
+    private var background: some View {
+        let paperShape = RoundedRectangle(cornerRadius: theme.cornerRadius)
+        let paperSurface = paperShape.fill(theme.palette.surface.main)
+        
+        return ZStack {
+            if hasShadow {
+                paperSurface.shadow(radius: 4)
+            }
+            else {
+                paperSurface
+            }
+            
+            if hasBorder {
+                paperShape.stroke(theme.palette.surface.content, lineWidth: 2)
+            }
+        }
+    }
+}
+
+#if DEBUG
+struct UrbanPaper_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            cardExample(border: false)
+                .padding()
+                .preferredColorScheme(.light)
+                .previewLayout(.sizeThatFits)
+                .previewDisplayName("UrbanPaper (Light Mode)")
+            
+            cardExample(border: true)
+                .padding()
+                .preferredColorScheme(.dark)
+                .previewLayout(.sizeThatFits)
+                .previewDisplayName("UrbanPaper (Dark Mode)")
+        }
+    }
+    
+    static func cardExample(border hasBorder: Bool) -> some View {
+        return UrbanPaper(border: hasBorder) {
+            VStack(spacing: 0) {
+                VStack(spacing: 16) {
+                    VStack(alignment: .leading) {
+                        Text("Tokyo")
+                            .font(.title)
+                        Text("Mon, 12:30 PM, Sunny")
+                            .font(.subheadline)
+                            .fontWeight(.medium)
+                            .foregroundColor(.secondary)
+                    }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    HStack(alignment: .center) {
+                        Text("23ºC")
+                        Spacer()
+                        Image(systemName: "sun.max.fill")
+                            .renderingMode(.original)
+                    }
+                    .font(.system(size: 72))
+                }
+                    .padding()
+                Divider()
+                UrbanButton("Expand") {}
+                    .padding(8)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+        }
+            .frame(width: 320)
+    }
+}
+#endif

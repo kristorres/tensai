835181a4fd36299b9d53ed82c6970ffec1b126f4
Kris Torres <torres-kris@outlook.com>
2022-03-25 15:46:48 -0700

    Convert UrbanPaper to a view modifier

--- Tensai/Urban/Modifiers/View+UrbanPaper.swift
+++ Tensai/Urban/Modifiers/View+UrbanPaper.swift
@@ -1,40 +1,42 @@
 import SwiftUI
 
-/// A simple paper element that displays content.
-struct UrbanPaper<Content>: View where Content: View {
+extension View {
     
-    /// Creates a paper element with the given content.
+    /// Displays the view on a paperlike surface.
     ///
     /// - Parameter hasBorder: Indicates whether the paper element has a border.
     ///                        The default is `false`.
     /// - Parameter hasShadow: Indicates whether the paper element has a shadow.
     ///                        The default is `true`.
-    /// - Parameter content:   The closure to render the content of the paper
-    ///                        element.
-    init(
+    ///
+    /// - Returns: The modified view.
+    func urbanPaper(
         border hasBorder: Bool = false,
-        shadow hasShadow: Bool = true,
-        content: @escaping () -> Content
-    ) {
-        self.hasBorder = hasBorder
-        self.hasShadow = hasShadow
-        self.content = content
+        shadow hasShadow: Bool = true
+    ) -> some View {
+        return modifier(UrbanPaper(hasBorder: hasBorder, hasShadow: hasShadow))
     }
+}
+
+/// A simple view modifier that displays content on a paperlike surface.
+fileprivate struct UrbanPaper: ViewModifier {
     
     /// Indicates whether the paper element has a border.
-    private let hasBorder: Bool
+    let hasBorder: Bool
     
     /// Indicates whether the paper element has a shadow.
-    private let hasShadow: Bool
-    
-    /// The closure to render the content of the paper element.
-    private let content: () -> Content
+    let hasShadow: Bool
     
     /// The app theme.
     @Environment(\.theme) private var theme
     
-    var body: some View {
-        content()
+    /// Returns the current body of the caller.
+    ///
+    /// - Parameter content: The content view.
+    ///
+    /// - Returns: The caller’s body.
+    func body(content: Content) -> some View {
+        return content
             .foregroundColor(theme.palette.surface.content)
             .background(theme.palette.surface.main)
             .clipShape(shape)
@@ -76,36 +78,34 @@ struct UrbanPaper_Previews: PreviewProvider {
     }
     
     static func cardExample(border hasBorder: Bool) -> some View {
-        return UrbanPaper(border: hasBorder) {
-            VStack(spacing: 0) {
-                VStack(spacing: 16) {
-                    VStack(alignment: .leading) {
-                        Text("Tokyo")
-                            .font(.title)
-                        Text("Mon, 12:30 PM, Sunny")
-                            .font(.subheadline)
-                            .fontWeight(.medium)
-                            .foregroundColor(.secondary)
-                    }
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    HStack(alignment: .center) {
-                        Text("23ºC")
-                        Spacer()
-                        Image(systemName: "sun.max.fill")
-                            .renderingMode(.original)
-                    }
-                    .font(.system(size: 72))
+        return VStack(spacing: 0) {
+            VStack(spacing: 16) {
+                VStack(alignment: .leading) {
+                    Text("Tokyo")
+                        .font(.title)
+                    Text("Mon, 12:30 PM, Sunny")
+                        .font(.subheadline)
+                        .fontWeight(.medium)
+                        .foregroundColor(.secondary)
                 }
-                    .padding()
-                Divider()
-                Button(action: {}) {
-                    Text("Expand").fontWeight(.semibold)
-                }
-                    .buttonStyle(.urban())
-                    .padding(8)
                     .frame(maxWidth: .infinity, alignment: .leading)
+                HStack(alignment: .center) {
+                    Text("23ºC")
+                    Spacer()
+                    Image(systemName: "sun.max.fill").renderingMode(.original)
+                }
+                    .font(.system(size: 72))
             }
+                .padding()
+            Divider()
+            Button(action: {}) {
+                Text("Expand").fontWeight(.semibold)
+            }
+                .buttonStyle(.urban())
+                .padding(8)
+                .frame(maxWidth: .infinity, alignment: .leading)
         }
+            .urbanPaper(border: hasBorder)
             .frame(width: 320)
     }
 }

e83ba3e57335afdd53066c166edbcbdf511d3670
Kris Torres <torres-kris@outlook.com>
2022-04-09 15:38:17 -0700

    Import my Urban SwiftUI design library
    Urban is a SwiftUI design library that I’m implementing and planning to
    use across all my personal Apple app projects.
    
    NOTE: This is still a work in progress.

--- Tensai/Themes/Trebek.swift
+++ Tensai/Themes/Trebek.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import Urban
 
 extension UrbanTheme {
     
@@ -6,22 +7,10 @@ extension UrbanTheme {
     static let trebek: UrbanTheme = {
         var theme = UrbanTheme()
         
-        theme.palette.primary.main = .dynamic(
-            light: Color(red: 0.0902, green: 0.3922, blue: 0.8549),
-            dark: Color(red: 0.6431, green: 0.7647, blue: 0.9569)
-        )
-        theme.palette.secondary.main = .dynamic(
-            light: Color(red: 1, green: 0.6784, blue: 0),
-            dark: Color(red: 1, green: 0.8667, blue: 0.5059)
-        )
-        theme.palette.surface.main = .dynamic(
-            light: .white,
-            dark: Color(red: 0.1686, green: 0.1804, blue: 0.2471)
-        )
-        theme.palette.background.main = .dynamic(
-            light: Color(red: 0.6235, green: 0.8471, blue: 1),
-            dark: Color(red: 0.0431, green: 0.1176, blue: 0.2353)
-        )
+        theme.palette.primary.main = Color("trebek-primary-main")
+        theme.palette.secondary.main = Color("trebek-secondary-main")
+        theme.palette.surface.main = Color("trebek-surface-main")
+        theme.palette.background.main = Color("trebek-background-main")
         
         theme.typography.button = .custom("Barlow Condensed Medium", size: 28)
         
